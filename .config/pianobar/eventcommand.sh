e33fd3726d1cc5046cbb84d642719f8e1e46fa0d
Harrison Powers <fijimunkii@gmail.com>
2014-03-21 15:54:51 -0400

    keep it pretty, keep it safe

--- .config/pianobar/eventcommand.sh
+++ .config/pianobar/eventcommand.sh
@@ -5,14 +5,14 @@ ctlf="$root/ctl"
 lyricsfile="$root/lyrics.sh"
 
 while read L; do
-        k="`echo "$L" | cut -d '=' -f 1`"
-        v="`echo "$L" | cut -d '=' -f 2`"
-        export "$k=$v"
+    k="`echo "$L" | cut -d '=' -f 1`"
+    v="`echo "$L" | cut -d '=' -f 2`"
+    export "$k=$v"
 done < <(grep -e '^\(title\|artist\|album\|stationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|songDuration\|songPlayed\|coverArt\|stationCount\|station[0-9]\+\)=' /dev/stdin)
 
 case "$1" in
-        songstart)
-        echo -e "glyrc lyrics -a \"$artist\" -t \"$title\" -b \"$album\" -w /tmp/" > "$lyricsfile"
-    /usr/local/bin/terminal-notifier -subtitle  "Artist: $artist" -message "Album: $album" -title "$title"
+    songstart)
+      echo -e "glyrc lyrics -a \"$artist\" -t \"$title\" -b \"$album\" -w /tmp/" > "$lyricsfile"
+      /usr/local/bin/terminal-notifier -subtitle  "Artist: $artist" -message "Album: $album" -title "$title"
 ;;
 esac
