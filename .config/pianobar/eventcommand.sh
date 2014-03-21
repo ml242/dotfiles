695fb73c733b0d3037fdae26b8baeadf7c76ea3c
Harrison Powers <fijimunkii@gmail.com>
2014-03-21 15:43:39 -0400

    sensible variables

--- .config/pianobar/eventcommand.sh
+++ .config/pianobar/eventcommand.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-fold="$HOME/.config/pianobar"
-ctlf="$fold/ctl"
-nowplaying="$fold/lyrics.sh"
+root="$HOME/.config/pianobar"
+ctlf="$root/ctl"
+lyricsfile="$root/lyrics.sh"
+
 while read L; do
         k="`echo "$L" | cut -d '=' -f 1`"
         v="`echo "$L" | cut -d '=' -f 2`"
@@ -10,8 +11,8 @@ while read L; do
 done < <(grep -e '^\(title\|artist\|album\|stationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|songDuration\|songPlayed\|coverArt\|stationCount\|station[0-9]\+\)=' /dev/stdin)
 
 case "$1" in
-        songstart)
-        echo -e "glyrc lyrics -a \"$artist\" -t \"$title\" -b \"$album\" -w /tmp/" > "$nowplaying"
+  songstart)
+        echo -e "glyrc lyrics -a \"$artist\" -t \"$title\" -b \"$album\" -w /tmp/" > "$lyricsfile"
     /usr/local/bin/terminal-notifier -subtitle  "Artist: $artist" -message "Album: $album" -title "$title"
 ;;
 esac
