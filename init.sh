a8ae7b9ee3d078081b6588359f1c41178ee08831
Harrison Powers <fijimunkii@gmail.com>
2014-03-30 23:19:17 -0400

    configures osx just right

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 printf "installing things.."
 
 case $OSTYPE 
@@ -44,6 +43,106 @@ case $OSTYPE
     gem install test-kitchen
     gem install berkshelf
 
+    # Trackpad
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+    defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+    defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+    defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+    # Enables keyboard control in OSX dialogs
+    defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+    # Screensaver
+    defaults write com.apple.screensaver askForPassword -int 1
+    defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+    # Screenshot
+    defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+    # Finder
+    defaults write com.apple.finder AppleShowAllFiles -bool true
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    defaults write com.apple.finder QLEnableTextSelection -bool true
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+    defaults write com.apple.frameworks.diskimages skip-verify -bool true
+    defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+    defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+    defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+    defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+    defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+    # Save
+    defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+    defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+    # Printing
+    defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+    # Launching Updating and Crashing Software
+    defaults write com.apple.LaunchServices LSQuarantine -bool false
+    defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+    defaults write com.apple.CrashReporter DialogType -string "none"
+    defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+    # Trash
+    defaults write com.apple.finder WarnOnEmptyTrash -bool false
+    defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+    # Enables Airdrop to hackintoshes
+    defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+    # Disables the dashboard
+    defaults write com.apple.dashboard mcx-disabled -boolean NO
+
+    # Expose Corners
+    defaults write com.apple.dock wvous-tl-corner -int 6 # Top-Left: Display Off 
+    defaults write com.apple.dock wvous-tr-corner -int 2 # Top-Right: All Windows
+    defaults write com.apple.dock wvous-bl-corner -int 5 # Bot-Left: Screensaver
+    defaults write com.apple.dock wvous-br-corner -int 4 # Bot-Right: Desktop
+
+    # Safari
+    defaults write com.apple.Safari HomePage -string "about:blank"
+    defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+    defaults write com.apple.Safari \
+      com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+    defaults write com.apple.Safari ShowFavoritesBar -bool false
+    defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+    defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+    defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+    defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+    defaults write com.apple.Safari IncludeDevelopMenu -bool true
+    defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+    defaults write com.apple.Safari \
+      com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+    defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+    defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+    # Itunes
+    defaults write com.apple.iTunes show-store-link-arrows -bool false
+    defaults write com.apple.iTunes disableGeniusSidebar -bool true
+    defaults write com.apple.iTunes disablePingSidebar -bool true
+    defaults write com.apple.iTunes disablePing -bool true
+    defaults write com.apple.iTunes disableRadio -bool true
+    defaults write com.apple.iTunes NSUserKeyEquivalents \
+      -dict-add "Target Search Field" "@F"
+
+    # Time Machine
+    defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+    # TextEdit
+    defaults write com.apple.TextEdit RichText -int 0
+    defaults write com.apple.TextEdit PlainTextEncoding -int 4
+    defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+    # Disk Utility
+    defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+    defaults write com.apple.DiskUtility advanced-image-options -bool true
 
   ;; in linux-gnu)
 
